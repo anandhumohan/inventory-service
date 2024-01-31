3597c487d91f40512d48cf822f51c6f0281de85d
anandhumohan <anandhumohan5@gmail.com>
2024-01-31 05:18:37 +0000

    discovery client done

--- src/main/java/com/sci/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/sci/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package com.sci.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.sci.inventoryservice.model.Inventory;
 import com.sci.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
